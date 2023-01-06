e03df8e9558aec4ae3a788d93e9b4f832c22e749
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-06 15:27:42 -0500

    user can start a new game and results of previous game is visible after game ends

--- app.js
+++ app.js
@@ -11,13 +11,41 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        //draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        //draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //monster lost
+        this.winner = "player";
+      }
+    },
   },
   computed: {
     monsterBarStyles() {
+      //we cannot represent negatives so we must set it to 0
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -25,6 +53,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
     attackMonster() {
       //increase round
       this.currentRound++;
